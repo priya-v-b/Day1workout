24d7611281942963029286772ad8146492e4ae05
priya-v-b <priyavb2004@gmail.com>
2026-02-26 12:21:25 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,17 @@
+public class Grade {
+    public static void main(String[] args) {
+    int Grade=81;
+    if(Grade>=90){
+        System.out.println("A");
+    }
+    else if(Grade>=75&&Grade<=89){
+      System.out.println("B");
+    }
+    else if(Grade>=50&&Grade<=74){
+        System.out.println("C");
+    }
+    else{
+        System.out.println("Fail");
+    }
+}
+}
