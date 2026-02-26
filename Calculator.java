5c73aedaba35f66a8d769d40db2a9bebad6aab9e
priya-v-b <priyavb2004@gmail.com>
2026-02-26 12:12:47 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a=500;
+        int b=100;
+        //addition//
+        int c=a+b;
+        System.out.println("Addition:"+c);
+        //Substraction//
+       int d=a-b;
+       System.out.println("Substraction:"+d);
+       //Multiplication//
+       int e=a*b;
+       System.out.println("Multiplication:"+e); 
+       //Division//
+       int f=a/b;
+       System.out.println("Division:"+f);
+    }
+}
