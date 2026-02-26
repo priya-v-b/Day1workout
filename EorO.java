3eb2ca142794b1e005f3dffc4f147318e25d0125
priya-v-b <priyavb2004@gmail.com>
2026-02-26 12:23:11 +0530

    Add files via upload

---
+++ EorO.java
@@ -0,0 +1,11 @@
+public class EorO {
+    public static void main(String[] args) {
+        int num=40;
+        if(num%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");
+        }
+    }
+}
